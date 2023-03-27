2f2df10f32ce60beea01b74966a0839d0fe767da
Saumya <sdshah.ind@gmail.com>
2023-03-27 15:11:51 +0530

    First commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/ingenio/tutorial_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/ingenio/assn_2_saumya/devel;/home/ingenio/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/ingenio/assn_2_saumya/devel;/home/ingenio/catkin_ws/devel;/opt/ros/noetic' != '' else []
---
+++ build/mobile_robot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ingenio/tutorial_ws/src/mobile_robot
+# Build directory: /home/ingenio/tutorial_ws/build/mobile_robot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
