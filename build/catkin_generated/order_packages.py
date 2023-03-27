e5df2995ff46991c762c5b756f6dec0909ccbf92
Saumya <sdshah.ind@gmail.com>
2023-03-27 15:25:13 +0530

    Workspace renamed

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/ingenio/tutorial_ws/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/ingenio/assn_2_saumya/devel;/home/ingenio/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/ingenio/assn_2_saumya/devel;/home/ingenio/catkin_ws/devel;/opt/ros/noetic' != '' else []
+underlay_workspaces = '/home/ingenio/tutorial_ws/devel;/home/ingenio/assn_2_saumya/devel;/home/ingenio/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/ingenio/tutorial_ws/devel;/home/ingenio/assn_2_saumya/devel;/home/ingenio/catkin_ws/devel;/opt/ros/noetic' != '' else []
--- devel/_setup_util.py
+++ devel/_setup_util.py
@@ -268,7 +268,7 @@ if __name__ == '__main__':
 
         if not args.local:
             # environment at generation time
-            CMAKE_PREFIX_PATH = r'/home/ingenio/assn_2_saumya/devel;/home/ingenio/catkin_ws/devel;/opt/ros/noetic'.split(';')
+            CMAKE_PREFIX_PATH = r'/home/ingenio/tutorial_ws/devel;/home/ingenio/assn_2_saumya/devel;/home/ingenio/catkin_ws/devel;/opt/ros/noetic'.split(';')
         else:
             # don't consider any other prefix path than this one
             CMAKE_PREFIX_PATH = []
